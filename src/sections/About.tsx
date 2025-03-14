a9a9d76308f7fe8612a0bf891af1ce99cd64ce61
AbderrahmaneRaquibi <a.raquibi4554@uca.ac.ma>
2025-03-14 22:42:15 +0000

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -181,21 +181,23 @@ export const AboutSection = () => {
               </div>
             </Card>
             <Card className="h-[320px] relative md:col-span-2 lg:col-span-1">
-              <Image
-                src={MapImage}
-                alt="Map Image"
-                className="h-full w-full object-cover object-left-top"
-              />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
-                <Image
-                  src="/favicon.ico"
-                  alt="Smiling Memoji"
-                  className="size-16"
-                />
-              </div>
-            </Card>
+  <Image
+    src={MapImage}
+    alt="Map Image"
+    className="h-full w-full object-cover object-left-top"
+  />
+  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
+    <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+    <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+    <div className="absolute inset-0 flex justify-center items-center">
+      <Image
+        src="/favicon.ico"
+        alt="Smiling Memoji"
+        className="size-16"
+      />
+    </div>
+  </div>
+</Card>
           </div>
         </div>
       </div>
