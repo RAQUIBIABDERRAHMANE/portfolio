7695a75ce7d5ddb6c7c4da8d416bed02dc0aa9a3
ABDERRAHMANE_RAQUIBI <a.raquibi4554@uca.ac.ma>
2025-05-31 16:16:00 +0100

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en\" className={inter.variable}>
       <head>
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
