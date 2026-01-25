139d655999eca085df385e6b31ebef3e1968b6b0
ABDERRAHMANE_RAQUIBI <a.raquibi4554@uca.ac.ma>
2026-01-25 15:04:29 +0000

    add shadcn script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,6 +117,11 @@ export default function RootLayout({
             }),
           }}
         />
+        <script
+          async
+          crossOrigin="anonymous"
+          src="https://tweakcn.com/live-preview.min.js"
+        />
       </head>
       <body className={inter.className}>
         <PWAInstaller />
